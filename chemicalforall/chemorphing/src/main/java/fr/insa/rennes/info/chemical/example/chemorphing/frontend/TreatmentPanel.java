b75036b63abb52634484caf75d754cc9b62e6731
Arthur Templé <arthur.temple@gmail.com>
2011-10-20 23:26:42 +0200

    [Chemorphing] Better layout for 1st panel

--- chemicalforall/chemorphing/src/main/java/fr/insa/rennes/info/chemical/example/chemorphing/frontend/TreatmentPanel.java
+++ chemicalforall/chemorphing/src/main/java/fr/insa/rennes/info/chemical/example/chemorphing/frontend/TreatmentPanel.java
@@ -1,11 +1,12 @@
 package fr.insa.rennes.info.chemical.example.chemorphing.frontend;
 
+import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,10 +25,12 @@ public class TreatmentPanel extends JPanel {
 	public TreatmentPanel(){
 		super();
 		
-		this.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 20));
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
 		this.add(Box.createRigidArea(new Dimension(0, 15)));
-		this.add(new JLabel(Globals.IMG_NUMBER_PROMPT));
+		JLabel jl = new JLabel(Globals.IMG_NUMBER_PROMPT);
+		jl.setAlignmentX(Component.CENTER_ALIGNMENT);
+		this.add(jl);
 		
 		//TODO tell where the pool is and get its size, then fill tab with consecutive integers
 		Integer[] integers = new Integer[]{2, 3};
@@ -46,6 +49,7 @@ public class TreatmentPanel extends JPanel {
 				// TODO Launch morphing
 			}
 		});
+		jb.setAlignmentX(Component.CENTER_ALIGNMENT);
 		jb.setPreferredSize(new Dimension(120, 30));
 		jb.setMaximumSize(new Dimension(120, 30));
 		this.add(jb);
