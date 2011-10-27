bdac165dfad24dab19c1cdb477e7c0ae15b58bf9
Arthur Templé <arthur.temple@gmail.com>
2011-10-27 05:14:38 +0200

    [Chemorphing] Removed old constants for good

--- chemicalforall/chemorphing/src/main/java/fr/insa/rennes/info/chemical/example/chemorphing/backend/Globals.java
+++ chemicalforall/chemorphing/src/main/java/fr/insa/rennes/info/chemical/example/chemorphing/backend/Globals.java
@@ -11,42 +11,6 @@ import java.util.regex.Pattern;
 
 
 public class Globals {
-//
-//	public static final String POOL_FOLDER = "pool";
-//	
-//	public static final String POOL_INDEX = POOL_FOLDER + "/index.json";
-//	
-//	public static final String TITLE = "Chemical Image Morpher";
-//	
-//	public static final String TREATMENT_TITLE = "Traitement";
-//	
-//	public static final String RESULT_TITLE = "Résultat du morphing";
-//	
-//	public static final String POOL_TITLE = "Piscine d'images";
-//
-//	public static final String ADD_TITLE = "Ajouter une nouvelle image";
-//
-//	public static final String IMG_NUMBER_PROMPT = "Nombre d'images à traiter";
-//	
-//	public static final String MORPH_BUTTON = "Morph !";
-//	
-//	public static final String SELECT_BUTTON = "Sélectionner une image à ajouter";
-//	
-//	public static final String ADD_BUTTON = "Ajouter à la piscine !";
-//	
-//	public static final int WINDOW_WIDTH = 800;
-//	
-//	public static final int WINDOW_HEIGHT = 600;
-//
-//	public static final String FULLSCREEN_BUTTON = "Visualiser en plein écran";
-//
-//	public static final String EXPORT_BUTTON = "Exporter au format GIF";
-//
-//	public static final String BACK_BUTTON = "Retour en mode fenêtré";
-//
-//	public static final int EXPORT_SECONDS_BETWEEN_FRAMES = 1;
-//	
-	
 	
 	
 	private static final String INI_FILE = "settings.ini";
