e6b67b3cb8eb44927eb36744e9d06c6565aa6946
ArthurTemple <arthur.temple@gmail.com>
2011-12-23 00:00:05 +0100

    BitHeaven stubs should now at least be compilable

--- chemicalforall/bitheaven/src/main/java/fr/insa/rennes/info/chemical/example/bitheaven/controller/ControllerFoo.java
+++ chemicalforall/bitheaven/src/main/java/fr/insa/rennes/info/chemical/example/bitheaven/controller/ControllerFoo.java
@@ -2,4 +2,12 @@ package fr.insa.rennes.info.chemical.example.bitheaven.controller;
 
 public class ControllerFoo {
 
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 }
