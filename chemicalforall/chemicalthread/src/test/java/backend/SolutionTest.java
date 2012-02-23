ea8a5c0c9a2976d20e6c4b46565c9afc4b071821
ArthurTemple <arthur.temple@gmail.com>
2012-02-23 19:52:21 +0100

    Some more tests for Solution

--- chemicalforall/chemicalthread/src/test/java/backend/SolutionTest.java
+++ chemicalforall/chemicalthread/src/test/java/backend/SolutionTest.java
@@ -1,7 +1,17 @@
 package backend;
-import fr.insa.rennes.info.chemical.backend.Solution;
-import fr.insa.rennes.info.chemical.user.ReactionRule;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 import junit.framework.TestCase;
+import fr.insa.rennes.info.chemical.backend.Dontreact;
+import fr.insa.rennes.info.chemical.backend.InertEvent;
+import fr.insa.rennes.info.chemical.backend.Solution;
+import fr.insa.rennes.info.chemical.backend.Solution.Strategy;
+import fr.insa.rennes.info.chemical.user.InertEventListener;
+import fr.insa.rennes.info.chemical.user.ReactionRule;
+import fr.insa.rennes.info.chemical.user.ReactionRule.Multiplicity;
 
 /**
  * @author ArthurTemple
@@ -32,6 +42,16 @@ public class SolutionTest extends TestCase {
 	 */
 	protected void tearDown() throws Exception {
 		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link Solution#Solution(Strategy)}.
+	 */
+	public void testConstructor() {
+		testSolution = new Solution(Strategy.ORDERED);
+		boolean verification = true;
+		verification &= testSolution.isEmpty();
+		assertTrue(verification == true);
 	}
 
 	/**
@@ -79,6 +99,113 @@ public class SolutionTest extends TestCase {
 		testSolution.add(barSol);
 		assertTrue(this.testSolution.contains(barSol));
 	}
+
+	/**
+	 * Test method for {@link Solution#addAll(Collection)}.
+	 */
+	public void testAddAll() {
+		boolean test = true;
+		Collection<Object> coll = new ArrayList<Object>();
+		coll.add(12);
+		coll.add(new Solution());
+		coll.add(new Object());
+		coll.add(new LinkedList<Byte>());
+		coll.add(new boolean[]{true, false, true, true});
+		
+		testSolution.addAll(coll);
+		
+		for(Object elem : coll){
+			test &= testSolution.contains(elem);
+		}
+
+		assertTrue(test);
+	}
+
+	/**
+	 * Test method for {@link Solution#containsAll(Collection)}.
+	 */
+	public void testContainsAll() {
+		Collection<Object> coll = new ArrayList<Object>();
+		
+		coll.add(12);
+		testSolution.add(12);
+		
+		Solution subSol = new Solution();
+		coll.add(subSol);
+		testSolution.add(subSol);
+		
+		Object foo = new Object();
+		coll.add(foo);
+		testSolution.add(foo);
+		
+		List<?> subColl = new LinkedList<Byte>();
+		coll.add(subColl);
+		testSolution.add(subColl);
+		
+		boolean[] subTab = new boolean[]{true, false, true, true};
+		coll.add(subTab);
+		testSolution.add(subTab);
+		
+		assertTrue(testSolution.containsAll(coll));
+	}
+
+	/**
+	 * Test method for {@link Solution#addInertEventListener(InertEventListener)}.
+	 */
+	public void testAddInertEventListener() {
+		testSolution = new Solution();
+		
+		testSolution.addInertEventListener(new InertEventListener() {
+			
+			public void isInert(InertEvent e) {
+				assertTrue(true);
+			}
+		});
+		
+		testSolution.react();
+		
+	}
+
+	/**
+	 * Test method for {@link Solution#clear()}.
+	 */
+	public void testClear() {
+		testSolution.clear();
+		
+		assertTrue(testSolution.isEmpty());
+		
+	}
+
+	/**
+	 * Test method for {@link Solution#deleteReaction(ReactionRule)}.
+	 */
+	public void testDeleteReaction() {
+		testSolution = new Solution();
+		ReactionRule rr = new ReactionRule() {
+			
+			public Multiplicity getMultiplicity() {
+				return Multiplicity.ONE_SHOT;
+			}
+			
+			public boolean computeSelect() {
+				return true;
+			}
+			
+			public Object[] computeResult() {
+				return new Object[]{1, 2, 3};
+			}
+		};
+		testSolution.add(rr);
+		testSolution.addInertEventListener(new InertEventListener() {
+			
+			public void isInert(InertEvent e) {
+				assertTrue(testSolution.isEmpty());
+			}
+		});
+		
+	}
+	
+	
 
 
 }
