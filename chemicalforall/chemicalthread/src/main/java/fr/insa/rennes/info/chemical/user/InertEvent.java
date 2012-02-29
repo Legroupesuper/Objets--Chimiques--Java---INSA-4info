37e5bdd14cd1d6b51d35ff983a7db649d7a2b627
Antoine <antoine.guellier@insa-rennes.fr>
2012-02-29 17:47:15 +0100

    Rectification répartition

---
+++ chemicalforall/chemicalthread/src/main/java/fr/insa/rennes/info/chemical/user/InertEvent.java
@@ -0,0 +1,11 @@
+package fr.insa.rennes.info.chemical.user;
+
+import java.util.EventObject;
+
+public class InertEvent extends EventObject{
+
+	public InertEvent(Object source) {
+		super(source);
+	}
+
+}
