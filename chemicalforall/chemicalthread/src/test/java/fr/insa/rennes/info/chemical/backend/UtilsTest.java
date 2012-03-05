3e43b3f17cfd79d42e1c9c6f0dcbc2878b443551
Arthur Templé <arthur.temple@gmail.com>
2012-03-05 23:33:13 +0100

    Two tests for Utils

--- chemicalforall/chemicalthread/src/test/java/fr/insa/rennes/info/chemical/backend/UtilsTest.java
+++ chemicalforall/chemicalthread/src/test/java/fr/insa/rennes/info/chemical/backend/UtilsTest.java
@@ -1,7 +1,11 @@
 package fr.insa.rennes.info.chemical.backend;
 
+import java.lang.reflect.Field;
+import java.util.logging.Logger;
+
 import junit.framework.TestCase;
 import fr.insa.rennes.info.chemical.user.ReactionRule;
+import fr.insa.rennes.info.chemical.user.ReactionRule.Multiplicity;
 
 /**
  * @author ArthurTemple
@@ -9,6 +13,8 @@ import fr.insa.rennes.info.chemical.user.ReactionRule;
  */
 
 public class UtilsTest extends TestCase {
+	
+	private String packageName = "fr.insa.rennes.info.chemical";
 	
 	/**
 	 * @param name
@@ -32,9 +38,43 @@ public class UtilsTest extends TestCase {
 	}
 
 	/**
-	 * Test method for {@link Utils#fooTest()}.
+	 * Test method for {@link Utils#logger}.
 	 */
-	public void testFooTest() {
-		assertTrue(true);
+	public void testLogger() {
+		assertFalse(Utils.logger.equals(Logger.getAnonymousLogger()));
+		assertTrue(Utils.logger.equals(Logger.getLogger(packageName)));
+	}
+
+	/**
+	 * Test method for {@link Utils#getMethodFromReactionRule()}.
+	 */
+	public void testGetMethodFromReactionRule() {
+		ReactionRule rr = new ReactionRule() {
+			
+			int foo;
+			
+			public void setFoo(int aFoo){
+				this.foo = aFoo;
+			}
+			
+			public Multiplicity getMultiplicity() {
+				return Multiplicity.ONE_SHOT;
+			}
+			
+			public boolean computeSelect() {
+				return true;
+			}
+			
+			public Object[] computeResult() {
+				return new Object[]{foo+12};
+			}
+		};
+		try {
+			assertTrue(Utils.getMethodFromReactionRule(rr, "set", rr.getClass().getDeclaredField("foo")).getName().equalsIgnoreCase("setFoo"));
+		} catch (NoSuchFieldException e) {
+			fail("NoSuchFieldException");
+		} catch (SecurityException e) {
+			fail("SecurityException");
+		}
 	}
 }
