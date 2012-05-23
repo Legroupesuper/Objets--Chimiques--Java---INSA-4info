ae888779bbc1bfe467f0f4f1eb230ec171dcb4b1
cboulang <chloe.boulanger@insa-rennes.fr>
2012-05-23 11:46:16 +0200

    Programme test de la bibliothèque Calcul des nombres premiers de 2 à N

---
+++ chemicalforall/usecases/src/main/java/nbPremiers/Test_NbPremiers.java
@@ -0,0 +1,30 @@
+package nbPremiers;
+
+import fr.insa.rennes.info.chemical.backend.Solution;
+import fr.insa.rennes.info.chemical.user.InertEvent;
+import fr.insa.rennes.info.chemical.user.InertEventListener;
+
+public class Test_NbPremiers {
+	public static long time = 0;
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		for(Integer i = 2; i<50; i++){
+			s.add(i);
+		}
+		s.add(new calculNbPremierRR());
+
+		s.addInertEventListener(new InertEventListener() {
+			public void isInert(InertEvent e) {
+				time = System.currentTimeMillis()-time;
+				System.out.println("\n\nRéaction terminée :");
+				System.out.println(e.getSource());
+				System.out.println("Time : "+time+"ms");
+			}
+		});
+
+		System.out.println(s);
+		time = System.currentTimeMillis();
+		s.react();
+	}
+}
---
+++ chemicalforall/usecases/src/main/java/nbPremiers/calculNbPremierRR.java
@@ -0,0 +1,37 @@
+package nbPremiers;
+
+import fr.insa.rennes.info.chemical.user.ReactionRule;
+
+public class calculNbPremierRR implements ReactionRule {
+
+	private Integer _a;
+	private Integer _b;
+
+	public Object[] computeResult() {
+		return new Object[] {this._b};
+	}
+
+	public boolean computeSelect() {
+		return (this._a%this._b == 0);
+	}
+
+	public Integer get_a(){
+		return this._a;
+	}
+	public Integer get_b(){
+		return this._b;
+	}
+
+	public Multiplicity getMultiplicity() {
+		return Multiplicity.INFINITY_SHOT;
+	}
+
+	public void set_a(Integer i){
+		this._a = i;
+	}
+
+	public void set_b(Integer i){
+		this._b = i;
+	}
+
+}
