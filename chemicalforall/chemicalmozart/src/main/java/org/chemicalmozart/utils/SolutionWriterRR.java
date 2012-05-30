e5b9b80f3dc61ab247a8f2c959d251c9b041dcf0
Antoine <antoine.guellier@insa-rennes.fr>
2012-05-30 14:43:46 +0200

    License "libchloe"

--- chemicalforall/chemicalmozart/src/main/java/org/chemicalmozart/utils/SolutionWriterRR.java
+++ chemicalforall/chemicalmozart/src/main/java/org/chemicalmozart/utils/SolutionWriterRR.java
@@ -1,3 +1,21 @@
+/* 
+	Copyright (C) 2012 Andreolli Cédric, Boulanger Chloé, Cléro Olivier, Guellier Antoine, Guilloux Sébastien, Templé Arthur
+
+    This file is part of libchloe.
+
+    libchloe is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libchloe is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+	
+    You should have received a copy of the GNU Lesser General Public License
+    along with libchloe.  If not, see <http://www.gnu.org/licenses/>
+*/
 package org.chemicalmozart.utils;
 
 import java.io.IOException;
--- chemicalforall/chemorphing/src/main/java/fr/insa/rennes/info/chemical/example/chemorphing/frontend/TreatmentPanel.java
+++ chemicalforall/chemorphing/src/main/java/fr/insa/rennes/info/chemical/example/chemorphing/frontend/TreatmentPanel.java
@@ -1,107 +1,18 @@
 /* 
 	Copyright (C) 2012 Andreolli Cédric, Boulanger Chloé, Cléro Olivier, Guellier Antoine, Guilloux Sébastien, Templé Arthur
 
-    This file is part of ChemicalLibSuper.
+    This file is part of libchloe.
 
-    ChemicalLibSuper is free software: you can redistribute it and/or modify
+    libchloe is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    ChemicalLibSuper is distributed in the hope that it will be useful,
+    libchloe is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 	
     You should have received a copy of the GNU Lesser General Public License
-    along with ChemicalLibSuper.  If not, see <http://www.gnu.org/licenses/>
+    along with libchloe.  If not, see <http://www.gnu.org/licenses/>
 */
-package fr.insa.rennes.info.chemical.example.chemorphing.frontend;
-
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerListModel;
-
-import fr.insa.rennes.info.chemical.example.chemorphing.backend.Globals;
-import fr.insa.rennes.info.chemical.example.chemorphing.middleend.MorphManager;
-import fr.insa.rennes.info.chemical.example.chemorphing.middleend.PoolManager;
-
-public class TreatmentPanel extends JPanel {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 5692640131194865714L;
-	
-	public TreatmentPanel(){
-		super();
-		
-		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		
-		initPanel();
-	}
-	
-	public void launchMorphing(int nb){
-		MorphManager.morph(nb);
-	}
-	
-	public void refreshPanel(){
-
-		this.removeAll();
-		
-		this.initPanel();
-		
-	}
-	
-	private final void initPanel(){
-		
-		this.add(Box.createRigidArea(new Dimension(0, 50)));
-		JLabel jl = new JLabel(Globals.getSetting("IMG_NUMBER_PROMPT"));
-		jl.setAlignmentX(Component.CENTER_ALIGNMENT);
-		this.add(jl);
-		
-		//get pool size, then fill tab with consecutive integers or show warning message
-		int poolSize = PoolManager.getPool().size();
-		
-		if(poolSize > 1){
-			Integer[] integers = new Integer[poolSize - 1];
-			for(int i=2 ; i<=poolSize ; ++i){
-				integers[i-2] = i;
-			}
-			
-			SpinnerListModel integersModel = new SpinnerListModel(integers);
-			final JSpinner spinner = new JSpinner(integersModel);
-			spinner.setPreferredSize(new Dimension(40, 30));
-			spinner.setMaximumSize(new Dimension(40, 30));
-
-			this.add(Box.createRigidArea(new Dimension(0, 15)));
-			this.add(spinner);
-
-			this.add(Box.createRigidArea(new Dimension(0, 15)));
-			JButton jb = new JButton(Globals.getSetting("MORPH_BUTTON"));
-			jb.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					launchMorphing((Integer) spinner.getValue());
-				}
-			});
-			jb.setAlignmentX(Component.CENTER_ALIGNMENT);
-			jb.setPreferredSize(new Dimension(120, 30));
-			jb.setMaximumSize(new Dimension(120, 30));
-			this.add(jb);
-		} else {
-			this.add(Box.createRigidArea(new Dimension(0, 15)));
-			this.add(new JLabel(Globals.getSetting("NOT_ENOUGH_IMAGES_PROMPT")));
-		}
-		
-	}
-
-}
